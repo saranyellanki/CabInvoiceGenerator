e6a6f612da4cfc27b9008998ee2200983314fc3a
saranyellanki <saran.yellanki12@gmail.com>
2021-11-22 20:08:18 +0530

    Added: Multiple rides fare

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -11,4 +11,12 @@ public class InvoiceGenerator {
         if (totalFare < MINIMUM_FARE) return MINIMUM_FARE;
         return totalFare;
     }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for(Ride ride : rides){
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
+    }
 }
